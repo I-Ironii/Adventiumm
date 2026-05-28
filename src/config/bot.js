d232d37d45020242512018e3330794be0a83eb8e
Ironii <ironfurhighlands@gmail.com>
2026-05-28 10:38:45 -0600

    Change bot activity type and brand colors
    Updated bot activity type and modified brand colors.

--- src/config/bot.js
+++ src/config/bot.js
@@ -27,7 +27,7 @@ export const botConfig = {
         // Text users will see (example: "Playing /help | Titan Bot").
         name: "Sniffing out some rabbits, actually.",
         // Activity type number (0 = Playing).
-        type: 4, 
+        type: 0, 
       },
     ],
   },
@@ -88,8 +88,8 @@ export const botConfig = {
   embeds: {
     colors: {
       // Main brand colors.
-      primary: "#336699", 
-      secondary: "#2F3136", 
+      primary: "##ffc40c", 
+      secondary: "#6495ed ", 
 
       // Standard status colors for success/error/warning/info messages.
       success: "#57F287", 
